4686e2d2aea6a2d0933e207ea12077f180556c68
KimDongWook <kimdw3824@gmail.com>
2023-06-24 13:20:59 +0900

    프로젝트 시작

---
+++ lib/src/view/login.dart
@@ -0,0 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/src/view/resister.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final auth = FirebaseAuth.instance;
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  void signIn() {
+    auth.signInWithEmailAndPassword(
+        email: email.value.text.trim(), password: password.value.text.trim());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    email.dispose();
+    password.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: email,
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: password,
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: signIn,
+              child: const Icon(Icons.login),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const Resister()));
+              },
+              child: const Text('Resister'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/view/resister.dart
@@ -0,0 +1,69 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Resister extends StatefulWidget {
+  const Resister({super.key});
+
+  @override
+  State<Resister> createState() => _ResisterState();
+}
+
+class _ResisterState extends State<Resister> {
+  final auth = FirebaseAuth.instance;
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  void signUp() {
+    try {
+      auth
+          .createUserWithEmailAndPassword(
+              email: email.value.text, password: password.value.text)
+          .then((value) {
+        email.clear();
+        password.clear();
+        Navigator.of(context).pop();
+      });
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        debugPrint('password too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        debugPrint('The account already exists for that email.');
+      }
+    } catch (e) {
+      print('error');
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    email.dispose();
+    password.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: email,
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: password,
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: signUp,
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
